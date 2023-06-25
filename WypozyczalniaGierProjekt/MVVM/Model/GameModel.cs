1df2fb541677c1d96d5d0e6ded81f7175c21c783
Kacper Jóźwik <kacperjozwik12@gmail.com>
2023-06-25 21:28:13 +0200

    game database update

---
+++ WypozyczalniaGierProjekt/MVVM/Model/GameModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaGierProjekt.MVVM.Model
+{
+    internal class GameModel
+    {
+        public string GameName { get; set; }
+        public string AvailableForPurchase { get; set; }
+        public string AvailableForRental { get; set; }
+        public string PriceForPurchase { get; set; }
+        public string PriceForRental { get; set; }
+    }
+}
