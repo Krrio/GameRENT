e5a38f04a1aacae65e2db10c9a988fba3a594288
Kacper Jóźwik <kacperjozwik12@gmail.com>
2023-06-28 19:09:34 +0200

    userUpdate

---
+++ WypozyczalniaGierProjekt/MVVM/View/UserView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WypozyczalniaGierProjekt.MVVM.View
+{ 
+    /// <summary>
+    /// Logika interakcji dla klasy UserView.xaml
+    /// </summary>
+    public partial class UserView : UserControl
+    {
+        private string connectionString = "Server=(local)\\SQLEXPRESS; Database=MVVMLoginDb; Integrated Security=true";
+        public UserView()
+        {
+            ///InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Users";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                ///dataGrid.ItemsSource = dataTable.DefaultView;
+            }
+        }
+    }
+}
--- WypozyczalniaGierProjekt/MVVM/ViewModel/MainViewModel.cs
+++ WypozyczalniaGierProjekt/MVVM/ViewModel/MainViewModel.cs
@@ -14,12 +14,14 @@ namespace WypozyczalniaGierProjekt.MVVM.ViewModel
         public RelayCommand BagViewCommand { get; set; }
         public RelayCommand ConfirmViewCommand { get; set; }
         public RelayCommand CollectionViewCommand { get; set; }
+        public RelayCommand UserViewCommand { get; set; }
 
         public HomeViewModel HomeVM { get; set; }
         public DiscoveryViewModel DiscoveryVM { get; set; }
         public BagViewModel BagVM { get; set; }
         public ConfirmViewModel ConfirmVM { get; set; }
         public CollectionViewModel CollectionVM { get; set; }
+        public UserViewModel UserVM { get; set; }
 
         private object _currentView;
         public object CurrentView
@@ -39,6 +41,7 @@ namespace WypozyczalniaGierProjekt.MVVM.ViewModel
             BagVM = new BagViewModel();
             ConfirmVM = new ConfirmViewModel();
             CollectionVM = new CollectionViewModel();
+            UserVM = new UserViewModel();
 
             CurrentView = HomeVM;
 
@@ -65,6 +68,11 @@ namespace WypozyczalniaGierProjekt.MVVM.ViewModel
             CollectionViewCommand = new RelayCommand(o =>
             {
                 CurrentView = CollectionVM;
+            });
+
+            UserViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = UserVM;
             });
         }
     }
