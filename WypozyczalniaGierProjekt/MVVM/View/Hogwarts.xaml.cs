e8830ef6797559af79a335727362de2fda83f141
Kacper Jóźwik <kacperjozwik12@gmail.com>
2023-07-02 01:51:48 +0200

    Code adding information to DB about choosen game, preparing for connection to ShopPage and displaying choosen game in shop summary

--- WypozyczalniaGierProjekt/MVVM/View/Hogwarts.xaml.cs
+++ WypozyczalniaGierProjekt/MVVM/View/Hogwarts.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,30 @@ namespace WypozyczalniaGierProjekt.MVVM.View
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
             Window.GetWindow(this).Close();
+        }
+        private void AddToCartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Pobierz dane gry, które chcesz dodać do bazy danych
+            string tytul = "Hogwarts Leagcy";
+            decimal cena = 229.99m;
+            DateTime dataKupienia = DateTime.Now;
+
+            // Połącz z bazą danych
+            string connectionString = "Server=(local)\\SQLEXPRESS; Database=MVVMLoginDb; Integrated Security=true";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Wstaw dane do tabeli Koszyk
+                string query = "INSERT INTO Koszyk (Tytuł, Cena, DataKupienia) VALUES (@Tytuł, @Cena, @DataKupienia)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Tytuł", tytul);
+                command.Parameters.AddWithValue("@Cena", cena);
+                command.Parameters.AddWithValue("@DataKupienia", dataKupienia);
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
         }
     }
 }
---
+++ WypozyczalniaGierProjekt/Repositories/ShopBagBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaGierProjekt.Repositories
+{
+    internal class ShopBagBase
+    {
+        string connectionString;
+        SqlConnection connection;
+
+        public ShopBagBase()
+        {
+            connectionString = "Server=(local)\\SQLEXPRESS; Database=MVVMLoginDb; Integrated Security=true";
+            connection = new SqlConnection(connectionString);
+        }
+    }
+}
